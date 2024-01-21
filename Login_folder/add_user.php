1339fb45b0095e729ca2ca731e881f038bd0d67b
Jensen <jensenrenger25@gmail.com>
2024-01-21 18:04:17 +0100

    few changes made in the php file

--- Login_folder/add_user.php
+++ Login_folder/add_user.php
@@ -1,26 +1,30 @@
 <?php
 // voor verbinden 
-include('/var/www/connections/connections.php')
+    include('/var/www/connections/connections.php')
 
-$firstname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['firstname']));
-$lastname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['phonenumber']));
-$email = mysqli_real_escape_string($connection, htmlspecialchars($_POST['email']));
-$phonenumber = mysqli_real_escape_string($connection, htmlspecialchars($_POST['phonenumber']));
-$username = mysqli_real_escape_string($connection, htmlspecialchars($_POST['username']));
-$password = mysqli_real_escape_string($connection, htmlspecialchars($_POST['password']));
-$postalcode = mysqli_real_escape_string($connection, htmlspecialchars($_POST['postalcode']));
-$housenumber = mysqli_real_escape_string($connection, htmlspecialchars($_POST['housenumber']));
+    if isset($_POST['submit']) 
+    {
+        $firstname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['firstname']));
+        $lastname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['lastname']));
+        $email = mysqli_real_escape_string($connection, htmlspecialchars($_POST['email']));
+        $phonenumber = mysqli_real_escape_string($connection, htmlspecialchars($_POST['phonenumber']));
+        $username = mysqli_real_escape_string($connection, htmlspecialchars($_POST['username']));
+        $password = mysqli_real_escape_string($connection, htmlspecialchars($_POST['password']));
+        $postalcode = mysqli_real_escape_string($connection, htmlspecialchars($_POST['postalcode']));
+        $housenumber = mysqli_real_escape_string($connection, htmlspecialchars($_POST['housenumber']));
+    }
 
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 
 try {
-$sql = "INSERT INTO Users (FirstName, LastName, Email, Phonenumber, UserName, Password) Values 
-('$firstname', '$lastname', '$email ' , '$phonenumber', '$username', '$hashed_password') ";
+$sql = "INSERT INTO Users (id, FirstName, LastName, Email, Phonenumber, UserName, Password) Values 
+('0', $firstname', '$lastname', '$email ' , '$phonenumber', '$username', '$hashed_password') ";
 mysqli_query($connection, $sql);
 } catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
 }
+
 
 
 
