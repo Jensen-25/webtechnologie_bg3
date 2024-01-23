b230bd9ac33e5f89b9725a4b4cf1675a965de9a3
Jensen <jensenrenger25@gmail.com>
2024-01-23 16:53:59 +0100

    connectie gefixt

--- Login_folder/add_user.php
+++ Login_folder/add_user.php
@@ -1,48 +1,51 @@
 <?php
-// voor verbinden 
-    include('/var/www/connections/connections.php');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verbinding met de database (pas dit aan volgens jouw databaseconfiguratie)
+    $servername = "localhost";
+    $username = "gebruik1";
+    $password = "7xE1GCiNAmxbg7a";
+    $dbname = "Database_bg3";
 
-    if (isset($_POST['submit']))
-    {
-        $firstname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['firstname']));
-        $lastname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['lastname']));
-        $email = mysqli_real_escape_string($connection, htmlspecialchars($_POST['email']));
-        $phonenumber = mysqli_real_escape_string($connection, htmlspecialchars($_POST['phonenumber']));
-        $username = mysqli_real_escape_string($connection, htmlspecialchars($_POST['username']));
-        $password = mysqli_real_escape_string($connection, htmlspecialchars($_POST['password']));
-        $password_2 = mysqli_real_escape_string($connection, htmlspecialchars($_POST['password_2']));
-        $postalcode = mysqli_real_escape_string($connection, htmlspecialchars($_POST['postalcode']));
-        $housenumber = mysqli_real_escape_string($connection, htmlspecialchars($_POST['housenumber']));
+    $connection = new mysqli($servername, $username, $password, $dbname);
+
+    // Controleer op connectiefout
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
     }
-    // both passwords have to corresponds
-    if ($_POST['passsword'] != $_POST['password_2']) {
-        echo 'Passwords are not the same';
+
+    // Ontvang gegevens van het formulier
+    $firstname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['firstname']));
+    $lastname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['lastname']));
+    $email = mysqli_real_escape_string($connection, htmlspecialchars($_POST['email']));
+    $phonenumber = mysqli_real_escape_string($connection, htmlspecialchars($_POST['phonenumber']));
+    $username = mysqli_real_escape_string($connection, htmlspecialchars($_POST['username']));
+    $password = mysqli_real_escape_string($connection, htmlspecialchars($_POST['password']));
+    $password_2 = mysqli_real_escape_string($connection, htmlspecialchars($_POST['password_2']));
+
+    // Controleer of de wachtwoorden overeenkomen
+    if ($password !== $password_2) {
+        die("Error: Passwords do not match.");
     }
-    // every field should be filled in 
-    if (empty($firstname) || empty($lastname) || empty($email) || empty($phonenumber) || 
-        empty($username) || empty($password) || empty($password_2) || empty($postalcode) || empty($housenumber)) {
+
+    // Controleer of elk veld is ingevuld
+    if (empty($firstname) || empty($lastname) || empty($email) || empty($phonenumber) ||
+        empty($username) || empty($password) || empty($password_2)) {
         die("Error: All fields are required.");
     }
 
-
-
-
-try {
-
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    $sql = "INSERT INTO Users (id, FirstName, LastName, Email, Phonenumber, UserName, Password) Values 
-    ('0', $firstname', '$lastname', '$email ' , '$phonenumber', '$username', '$hashed_password') ";
-    mysqli_query($connection, $sql);
-} 
-catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
+
+    // SQL-query voor het invoegen van gegevens
+    $sql = "INSERT INTO Users (FirstName, LastName, Email, Phonenumber, UserName, Password)
+            VALUES ('$firstname', '$lastname', '$email', '$phonenumber', '$username', '$hashed_password')";
+
+    if ($connection->query($sql) === TRUE) {
+        echo "Record toegevoegd aan de database!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $connection->error;
+    }
+
+    // Sluit de databaseverbinding
+    $connection->close();
 }
-
-
-
-
-
-
-
-
 ?>
