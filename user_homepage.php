595c179a2efabd517308688948838e039722c462
Jensen <jensenrenger25@gmail.com>
2024-01-31 21:01:15 +0100

    include statement changed

--- user_homepage.php
+++ user_homepage.php
@@ -20,7 +20,7 @@ session_start();
     </head>
 
     <body>
-    <?php include_once 'cookies.php'; ?>
+    <?php include 'cookies.php'; ?>
         <!-- Slideshow container, based on slideshow tutorial from W3Schools https://www.w3schools.com/howto/default.asp -->
         <div class="slideshow-container">
 
