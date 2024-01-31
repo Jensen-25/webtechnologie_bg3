814b27771f608e990019e65c3132a5ee3db8f495
Amanda <amandajansen2001@gmail.com>
2024-01-31 13:32:33 +0100

    Fixed minor errors

--- FAQ/Navbar.js
+++ FAQ/Navbar.js
@@ -8,7 +8,7 @@ function createNavbar() {
       { text: 'Home', link: '../index.html' },
       { text: 'Products', link: '../Products_folder/main_products_page.html' },
       { text: 'Login', link: '../Login_folder/Login_screen.php' },
-      { text: 'Registration', link: '../Login_folder/registratiescherm.php' },
+      { text: 'Register', link: '../Login_folder/registratiescherm.php' },
       { text: 'Admin', link: '../Admin_folder/Admin_settings.php' },
       { text: 'Shopping Cart', link: '../Shoppingcart_folder/shoppingcart_page.html' },
     ];
