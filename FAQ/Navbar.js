51cb11e1f418f4bae66a9f7c1c4b78d884fc1a92
Jensen <jensenrenger25@gmail.com>
2024-01-31 13:02:05 +0100

    new nav

--- FAQ/Navbar.js
+++ FAQ/Navbar.js
@@ -4,7 +4,6 @@ function createNavbar() {
   const navBar = document.createElement('ul');
   navBar.classList.add('navbar');
 
-  if (getLoggedInUser() == null) {
   const navItems = [
     { text: 'Home', link: '../' },
     { text: 'Products', link: '../Products_folder/main_products_page.html' },
@@ -13,24 +12,6 @@ function createNavbar() {
     { text: 'Admin', link: '../Admin_folder/Admin_settings.php' },
     { text: 'Shopping Cart', link: '../Shoppingcart_folder/shoppingcart_page.html' },
   ];
-} else {
-  const navItems = [
-    { text: 'Home', link: '../' },
-    { text: 'Products', link: '../Products_folder/main_products_page.html' },
-    { text: 'Login', link: '../Login_folder/Login_screen.php' },
-    { text: 'Registration', link: '../Login_folder/registratiescherm.php' },
-    { text: 'Admin', link: '../Admin_folder/Admin_settings.php' },
-    { text: 'Shopping Cart', link: '../Shoppingcart_folder/shoppingcart_page.html' },
-  ];
-    // Check if a user is logged in
-    const loggedInUser = getLoggedInUser(); // Implement this function to retrieve the logged-in user
-
-    if (loggedInUser) {
-      // If a user is logged in, add Logout and display the username
-      navItems.push({ text: 'Logout', link: '../logout.php' });
-      navItems.push({ text: `Welcome, ${loggedInUser}`, link: '#' });
-    }
-  }
   
   navItems.forEach(item => {
     const listItem = document.createElement('li');
