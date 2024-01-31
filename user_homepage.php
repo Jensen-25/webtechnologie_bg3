20b0957797d5459f8dee880e207425f9887d2076
Jensen <jensenrenger25@gmail.com>
2024-01-31 16:14:04 +0100

    fixed

--- user_homepage.php
+++ user_homepage.php
@@ -16,7 +16,7 @@ session_start();
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <title> Homepage User </title>
             
-        <?php include '../Navbar_folder/Navbar_link.php'; ?>
+        <?php include 'Navbar_folder/Navbar_link.php'; ?>
     </head>
 
     <body>
