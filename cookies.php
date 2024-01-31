4b9b6615343be79dc14aa7bcc7ca1e85b7945d59
Jensen <jensenrenger25@gmail.com>
2024-01-31 20:58:16 +0100

    cookie message added

--- cookies.php
+++ cookies.php
@@ -1,4 +1,40 @@
 <!-- Cookie Popup -->
+<style> 
+/* Cookie Popup Styles */
+#cookie-popup {
+    display: none;
+    position: fixed;
+    bottom: 20px;
+    left: 20px;
+    width: 300px;
+    padding: 15px;
+    background-color: #ADD8E6;
+    color: #fff;
+    border-radius: 8px;
+    z-index: 100;
+    color: white;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+#cookie-popup p {
+    margin: 0;
+}
+
+#cookie-popup button {
+    background-color: #5cb85c;
+    color: #fff;
+    padding: 8px 12px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-top: 10px;
+}
+
+#cookie-popup button:hover {
+    background-color: #773cae;
+}
+</style>
+
 <div id="cookie-popup">
     <p>This website uses cookies to ensure you get the best experience on our website.</p>
     <button onclick="acceptCookies()">Got It!</button>
